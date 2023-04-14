7da73f46b582a0d69b71648cb9d9452fd805cca3
Corentin <92300620+CorentinVep@users.noreply.github.com>
2023-04-14 14:43:31 +0200

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,36 @@
+$(document).ready(function () {
+    $('#slide1').owlCarousel({
+        loop: true,
+        nav: true,
+        dots: false,
+        responsive: {
+            0: {
+                items: 1,
+            },
+        }
+    })
+});
+
+$(document).ready(function () {
+    $("li.active").removeClass("active");
+    $('a[href="' + location.pathname + '"]')
+      .closest("li")
+      .addClass("active");
+  });
+  
+  const nav = document.querySelector('.Navbar');
+  window.addEventListener('scroll', fixNav);
+  
+  function fixNav() {
+      if (window.scrollY > nav.offsetHeight + 150) {
+          nav.classList.add('navbar-active');
+      } else {
+          nav.classList.remove('navbar-active');
+      }
+  }
+
+  $(function(){
+    $(".to-cars").on("click",function(){
+        window.location = "https://www.taxi-orly.com/nos-navettes#cars";
+    });
+})
